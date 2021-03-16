ca42c29617e2f566cafbb74baab783eea4246e43
dylan-rinella <rinelladylan@aim.com>
2021-03-16 18:49:12 -0400

    basically mvp - 1 middleware

--- api/cars/cars-middleware.js
+++ api/cars/cars-middleware.js
@@ -1,15 +1,51 @@
-const checkCarId = (req, res, next) => {
-  // DO YOUR MAGIC
+const Cars = require('./cars-model')
+
+const checkCarId = async (req, res, next) => {
+  try {
+    const car = await Cars.getById(req.params.id)
+    if (!car) {
+      res.status(404).json({message: `car with id:${req.params.id} does not exist`})
+    } else {
+      console.log(`Checking Car ID:${req.params.id}`)
+      req.car = car
+      next()
+    }
+  } catch (error) {
+    next (error)
+  }
 }
 
 const checkCarPayload = (req, res, next) => {
-  // DO YOUR MAGIC
+  if (!req.body) {
+    res.status(400).json({message: `requires additional vehicle information`})
+  } else {
+    console.log('Payload meets criteria')
+    next()
+  }
 }
 
-const checkVinNumberValid = (req, res, next) => {
-  // DO YOUR MAGIC
+const checkVinNumberValid = async (req, res, next) => {
+  try {
+    const car = await Cars.getById(req.params.id)
+    if (!car.vin) {
+      res.status(400).json({message: `Please provide a valid VIN`})
+    } else {
+      console.log(`Vin: ${car.vin} is valid`)
+      req.car = car
+      next()
+    }
+  } catch (error) {
+    next(error)
+  }
 }
 
 const checkVinNumberUnique = (req, res, next) => {
   // DO YOUR MAGIC
+}
+
+
+module.exports = {
+  checkCarId,
+  checkCarPayload,
+  checkVinNumberValid,
 }
--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -2,9 +2,12 @@ const express = require('express')
 
 const Cars = require('./cars-model')
 
+//middleware
+const { checkCarId, checkCarPayload, checkVinNumberValid } = require('./cars-middleware')
+
 const router = express.Router()
 
-router.get('/', (req, res, next) => {
+router.get('/', checkCarPayload, (req, res, next) => {
   Cars.getAll()
     .then(car => {
       res.status(200).json(car)
@@ -12,7 +15,7 @@ router.get('/', (req, res, next) => {
     .catch(next)
 })
 
-router.get('/:id', (req, res, next) => {
+router.get('/:id', checkCarId, checkVinNumberValid, (req, res, next) => {
   Cars.getById(req.params.id)
     .then(car => {
       res.status(200).json(car)
@@ -20,7 +23,7 @@ router.get('/:id', (req, res, next) => {
     .catch(next)
 })
 
-router.post('/', (req, res, next) => {
+router.post('/', checkCarPayload, (req, res, next) => {
   Cars.create(req.body)
     .then(car => {
       res.status(201).json(car)
